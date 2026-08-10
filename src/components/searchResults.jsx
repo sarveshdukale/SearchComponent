439adabcee16c44263ac474cb04fc144e2462947
Sarvesh Dukale <sarveshdukale2004@gmail.com>
2026-08-10 23:47:50 +0530

    Search Result Secrion created

---
+++ src/components/searchResults.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function SearchResults() {
+
+    
+
+  return (
+      <>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero, earum.</p>
+      </>
+  )
+}
+
+export default SearchResults
