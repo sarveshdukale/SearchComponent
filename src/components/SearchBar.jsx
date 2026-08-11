03695429deecdd472b0b3ee43cb9e576397ba8a3
Sarvesh Dukale <sarveshdukale2004@gmail.com>
2026-08-11 23:53:07 +0530

    search bar logic

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { IoIosSearch } from 'react-icons/io';
 import { IoSearch } from 'react-icons/io5';
 
 function SearchBar() {
+    const [search , setSearch] = useState(" ")
   return (
       <>
           <div id="seracBarContainer">
               <IoIosSearch id='icon'/>
-              <input type="text" placeholder='Enter to Search...' />
+              <input type="text" value={search} placeholder='Enter to Search...' onChange={(e) => {
+                  setSearch(e.target.value)
+              }} />
+              
+              {console.log(search)
+              }
           </div>
+
+          
       </>
   )
 }
