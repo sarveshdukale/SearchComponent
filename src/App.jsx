80df6edbe5a0b2f457d27a8dcc36bd2a26fdf54e
Sarvesh Dukale <sarveshdukale2004@gmail.com>
2026-08-12 23:56:53 +0530

    add search state and send it to both component using props

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,12 @@ import SearchBar from './components/SearchBar';
 import SearchResults from './components/searchResults';
 
 function App() {
-  
+  const [search, setSearch] = useState(" ");
   return (
     <>
       <div id="searchContainer">
-        <SearchBar />
-        <SearchResults/>
+        <SearchBar search={search} setSearch={setSearch}/>
+        <SearchResults search={search} />
       </div>  
     </>
   )
